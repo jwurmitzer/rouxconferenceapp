502274291624553d8e11658d9b7ec98248ae50db
joe mitchell <joe_exp@yahoo.com>
2014-02-21 14:37:21 +0000

    Bootstrap Plain
    Bootstrap Plain

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/mysscript.js"></script>
+  </body>
+</html>
