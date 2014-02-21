c195dfa1032af770cf2d0355925f048514d4730a
joe mitchell <joe_exp@yahoo.com>
2014-02-21 19:19:26 +0000

    added breadcrumb

--- venuetravel.php
+++ venuetravel.php
@@ -12,12 +12,13 @@
 		<section class="container">
 			<div class="content row">
 				<section class="main col col-lg-8">
-
+					<?php include "_/components/php/header.php"; ?>
 				</section><!-- main -->
 				<section class="sidebar col col-lg-4">
 
 				</section><!-- sidebar -->
 			</div><!-- content -->
+				<?php include "_/components/php/footer.php"; ?>
 		</section><!-- container -->
 
     <script src="_/js/bootstrap.js"></script>
