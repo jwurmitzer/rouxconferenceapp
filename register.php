5d4c012864b2e616bff950ada34030df948cd7aa
joe mitchell <joe_exp@yahoo.com>
2014-02-21 15:53:28 +0000

    basic scaffold for website complete

--- register.php
+++ register.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Roux Academy Conference 2013 -- Home</title>
+    <title>Roux Academy Conference 2013 -- Register</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
   </head>
-  <body id="home">
+  <body id="register">
 
 		<section class="container">
 			<div class="content row">
